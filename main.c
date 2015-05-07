b33498ebe86ad876c96c82429e1b1879fb5e9bab
Felix Angell <mail@felixangell.com>
2015-05-07 08:40:52 +0100

    updated docs

--- main.c
+++ main.c
@@ -34,10 +34,10 @@ typedef enum {
 	MOV, // 7 -- mov reg_a, reg_b :: movs the value in reg_a to reg_b 
 	SET, // 8 -- set reg, val :: sets the reg to value
 	LOG, // 9 -- log a :: prints out a
-	IF, 
-	IFN,
-	GLD, // 10
-	GPT, // 11
+	IF,  // 10 -- if reg val ip :: if the register == val branch to the ip
+	IFN, // 11 -- ifn reg val ip :: if the register != val branch to the ip
+	GLD, // 10 -- gld reg :: loads a register to the stack
+	GPT, // 11 -- gpt reg :: pushes top of stack to the given register
 	NOP  // 12 -- nop :: nothing
 } Instructions;
 
